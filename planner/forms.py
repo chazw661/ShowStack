e43ecd441bfe39d008360ccf890853d018ea88ad
userName <userEmail>
2025-07-21 00:05:20 -0400

    Fix column mapping in console_detail and align with Input model fields

--- planner/forms.py
+++ planner/forms.py
@@ -3,15 +3,22 @@ from django import forms
 from django.forms import modelformset_factory
 from .models import Input
 
-class InputForm(forms.ModelForm):
+class ConsoleInputForm(forms.ModelForm):
     class Meta:
-        model = Input
+        model = ConsoleInput  # or whatever your model is called
         fields = [
-            'channel', 'label', 'device', 'output',
-            'dante', 'input_ch', 'source', 'group',
-            'dca', 'mute', 'direct_out', 'omni_in', 'omni_out'
+            'dante_number',
+            'channel',
+            'source',
+            'group',
+            'dca',
+            'mute',
+            'direct_out',
+            'omni_in',
+            'omni_out',
         ]
 
+
 InputFormSet = modelformset_factory(
-    Input, form=InputForm, extra=144, can_delete=False
+    Input, form=InputForm, extra=144, can_delete=True
 )
