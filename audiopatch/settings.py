0fee247f5372e4ff5e5a8ee82c2c0334b2400cef
Charlie Lawson <lawsonsounddesign@gmail.com>
2026-03-04 11:33:18 -0500

    Use MEDIA_ROOT env var for Railway volume path

--- audiopatch/settings.py
+++ audiopatch/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', str(BASE_DIR / 'media'))
 
 
 ROOT_URLCONF = 'audiopatch.urls'
